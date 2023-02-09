26a425af85b0bcd0ef0d6e63ade865296c0dc874
maria2332 <maria.ballesteros2004@gmail.com>
2023-02-09 12:39:28 +0100

    cambio

---
+++ ejercicio6/6.py
@@ -0,0 +1,19 @@
+"""
+Realiza una función separar(lista) que tome una lista de números enteros y devuelva dos listas ordenadas. La primera con los números pares y la segunda con los números impares.
+
+Por ejemplo:
+
+pares, impares = separar([6,5,2,1,7])
+
+print(pares)
+
+print(impares)
+
+[2, 6]
+
+[1, 5, 7]
+
+Sugerencia
+
+Para ordenar una lista automáticamente puedes utilizar el método .sort().
+"""
