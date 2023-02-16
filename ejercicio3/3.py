0f5211e8258224c20efcddcd1d133626eff27195
maria2332 <maria.ballesteros2004@gmail.com>
2023-02-16 09:46:24 +0100

    cambio

--- ejercicio3/3.py
+++
@@ -1,7 +0,0 @@
-"""
-Dadas dos listas, debes generar una tercera con todos los elementos que se repitan en ellas, pero no debe repetirse ningún elemento en la nueva lista:
-
-lista_1 = ["h",'o','l','a',' ', 'm','u','n','d','o']
-
-lista_2 = ["h",'o','l','a',' ', 'l','u','n','a']
-"""
