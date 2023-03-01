0b1368d734f3033b1cc0c328bfbad6a56d639f60
maria2332 <maria.ballesteros2004@gmail.com>
2023-03-01 17:01:54 +0100

    cambio

--- ejercicios/ejercicio1.py
+++ ejercicios/ejercicio1.py
@@ -6,3 +6,24 @@ Ayuda
 Para voltear una cadena rápidamente utilizando slicing podemos utilizar un tercer índice -1: cadena[::-1]
 cadena = "zeréP nauJ,01"
 """
+
+from ast import main 
+
+class Alumno:
+    def __init__(self, cadena):
+        self.cadena = cadena
+
+    @staticmethod
+    def formatear_cadena(cadena):
+        cadena_correcta = cadena[::-1]
+        nota, nombre = cadena_correcta.split(",")
+        return f"{nombre} ha sacado un {nota}"
+    
+# cadena = "zeréP nauJ,01"
+# alumno = Alumno(cadena)
+# print(alumno.formatear_cadena(cadena))
+
+if __name__ == "__main__":
+    cadena = input("Introduce una cadena: ")
+    alumno = Alumno(cadena)
+    print(alumno.formatear_cadena(cadena))
