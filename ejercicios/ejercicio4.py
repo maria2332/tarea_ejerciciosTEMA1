5e080dfa5d7a431e2f276ac8305eea1c46a44a88
maria2332 <maria.ballesteros2004@gmail.com>
2023-03-01 17:21:27 +0100

    cambio

--- ejercicios/ejercicio4.py
+++ ejercicios/ejercicio4.py
@@ -6,3 +6,15 @@ Sugerencia
 Para ordenar automáticamente una lista es posible utilizar el método .sort(), deberias probarlo.
 """
 
+def ordenar(lista):
+    lista.sort()
+    lista_2 = []
+    for i in lista:
+        lista_2.append(i[1])
+    return lista_2
+
+if __name__ == "__main__":
+    lista = [(2, 'Preparar ropa'), (4, 'Hacer la cama'), (3, 'Sacar al perro'), (1, 'Lavar los platos')]
+    print(ordenar(lista))
+
+    
