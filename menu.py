74bd4b5a7368145e2936b419edad53304b182725
maria2332 <maria.ballesteros2004@gmail.com>
2023-03-07 17:36:24 +0100

    cambio

--- menu.py
+++ menu.py
@@ -1,107 +1,104 @@
 import os
 import sys
 import re
-from colorama import *
-from termcolor import *
 import helpers
-from ejercicios import ejercicio1
-from ejercicios import ejercicio2
-from ejercicios import ejercicio3
-from ejercicios import ejercicio4
-from ejercicios import ejercicio5
-from ejercicios import ejercicio6
-from ejercicios import ejercicio7
+from ejercicios.ejercicio1 import Alumno 
+from ejercicios.ejercicio2 import num
+from ejercicios.ejercicio3 import listas
+from ejercicios.ejercicio4 import estructura_cola
+from ejercicios.ejercicio5 import descomponer
+from ejercicios.ejercicio6 import separar
+from ejercicios.ejercicio7 import agregar_una_vez
 
 
-init(autoreset=True)
 
 
 def iniciar():
     while True:
         helpers.limpiar_pantalla()
 
-        lineas=(Fore.BLUE +"="*50)
+        lineas=("="*50)
         print(lineas)
-        print(colored((("Bienvenido al menú de ejercicios de Python").upper()).center(50), "white", "on_blue"))
+        print(((("Bienvenido al menú de ejercicios de Python").upper()).center(50)))
         print(lineas)
-        print(colored(Fore.BLUE+"[1]"),"Ejercicio 1: Alumno")
-        print(colored(Fore.BLUE+"[2]"),"Ejercicio 2: Número mágico")
-        print(colored(Fore.BLUE+"[3]"),"Ejercicio 3: Listas")
-        print(colored(Fore.BLUE+"[4]"),"Ejercicio 4: Tareas")
-        print(colored(Fore.BLUE+"[5]"),"Ejercicio 5: Descomposicion")
-        print(colored(Fore.BLUE+"[6]"),"Ejercicio 6: Pares e impares")
-        print(colored(Fore.BLUE+"[7]"),"Ejercicio 7: Agregar una vez")
-        print(colored(Fore.RED+"[8]"),("SALIR"))
+        print(("[1]"),"Ejercicio 1: Alumno")
+        print(("[2]"),"Ejercicio 2: Número mágico")
+        print(("[3]"),"Ejercicio 3: Listas")
+        print(("[4]"),"Ejercicio 4: Tareas")
+        print(("[5]"),"Ejercicio 5: Descomposicion")
+        print(("[6]"),"Ejercicio 6: Pares e impares")
+        print(("[7]"),"Ejercicio 7: Agregar una vez")
+        print(("[8]"),("SALIR"))
         print(lineas)
 
-        opcion = input(colored(Fore.BLUE+"Elige una opción: ", "blue"))
+        opcion = input(("Elige una opción: "))
         helpers.limpiar_pantalla()
 
         if opcion == "1":
-            print(Back.CYAN+Fore.WHITE+"Ejercicio 1: Alumno")
+            print(B"Ejercicio 1: Alumno")
             #ejercicicios.alumno
-            ejercicio1.cadena=input(colored(Fore.BLUE+"Ingresa tu cadena corrupta: ", "blue"))
-            print(ejercicio1.formatear_cadena(ejercicio1.cadena))
+            Alumno.cadena=input(("Ingresa tu cadena corrupta: "))
+            print(Alumno.formatear_cadena(Alumno.cadena))
 
         
 
         elif opcion == "2":
-            print(Back.CYAN+Fore.WHITE+"Ejercicio 2: Número mágico")
-            n=int(input(colored(Fore.BLUE+"Ingresa un número: ", "blue")))
-            print(ejercicio2.numeromagico.num(n))
+            print("Ejercicio 2: Número mágico")
+            n=int(input(("Ingresa un número del 1 al 9: ")))
+            print(num(n))
 
         elif opcion == "3":
-            print(Back.CYAN+Fore.WHITE+"Ejercicio 3: Listas")
-            list1 =list(input(colored(Fore.BLUE+"Ingresa tu primera lista: ", "blue")))
-            list2 =list(input(colored(Fore.BLUE+"Ingresa tu segunda lista: ", "blue")))
-            print(ejercicio3(list1, list2))
+            print("Ejercicio 3: Listas")
+            list1 =list(input(("Ingresa tu primera lista: ")))
+            list2 =list(input(("Ingresa tu segunda lista: ")))
+            print(listas(list1, list2))
 
         elif opcion == "4":
-            print(Back.CYAN+Fore.WHITE+"Ejercicio 4: Tareas")
+            print("Ejercicio 4: Tareas")
             list_cola=[]
-            NumeroTareas = int(input(colored(Fore.BLUE+"Ingresa el número de tareas: ", "blue")))
+            NumeroTareas = int(input((F"Ingresa el número de tareas: ")))
             for i in range(NumeroTareas):
                 # Crear un diccionario
                 cola = {}
                 # Agregar elementos al diccionario
-                cola[f'tarea {i+1}'] = input(colored(Fore.BLUE+f'Ingresa la tarea {i+1}: ', "blue"))
-                cola[f'prioridad'] = input(colored(Fore.BLUE+f'Ingresa la prioridad de la tarea {i+1}: ', "blue"))
+                cola[f'tarea {i+1}'] = input((f'Ingresa la tarea {i+1}: '))
+                cola[f'prioridad'] = input((f'Ingresa la prioridad de la tarea {i+1}: '))
                 list_cola.append(cola)
-            ejercicio4(list_cola)
+            estructura_cola(list_cola)
             
         
         elif opcion == "5":
-            print(Back.CYAN+Fore.WHITE+"Ejercicio 5: Descomposicion")
-            n=input(colored(Fore.BLUE+"Ingresa un número: ", "blue"))
-            ejercicio5(n)
+            print("Ejercicio 5: Descomposicion")
+            n=input(("Ingresa un número: ", "blue"))
+            descomponer(n)
 
         elif opcion == "6":
-            print(Back.CYAN+Fore.WHITE+"Ejercicio 6: Pares e impares")
-            n=int(input(colored(Fore.BLUE+"Longitud de la lista: ", "blue")))
+            print("Ejercicio 6: Pares e impares")
+            n=int(input(("Longitud de la lista: ", "blue")))
             lista6=[]
             for i in range(n):
-                lista6.append(int(input(colored(Fore.BLUE+"Ingresa el número: ", "blue"))))
-            print(ejercicio6(lista6))
+                lista6.append(int(input(("Ingresa el número: "))))
+            print(separar(lista6))
 
 
         elif opcion == "7":
-            print(Back.CYAN+Fore.WHITE+"Ejercicio 7: Agregar una vez")
+            print("Ejercicio 7: Agregar una vez")
             # lista por inputs con ints y strings
             lista7= []
-            n = int(input(colored(Fore.BLUE+"Longitud de la lista: ", "blue")))
+            n = int(input(("Longitud de la lista: ")))
             for i in range(n):
-                lista7.append(input(colored(Fore.BLUE+"Ingresa el elemento de la lista: ", "blue")))
+                lista7.append(input(("Ingresa el elemento de la lista: ")))
                 if lista7[i].isnumeric():
                     lista7[i] = int(lista7[i])
-            elemento = input(colored(Fore.BLUE+"Ingresa el elemento: ", "blue"))
-            print(ejercicio7(lista7, elemento))
+            elemento = input(("Ingresa el elemento: "))
+            print(agregar_una_vez(lista7, elemento))
 
 
 
         elif opcion == "8":
-            print(colored(Fore.RED+"Saliendo..."))
+            print((F"Saliendo..."))
             break
 
-        input(colored(Fore.CYAN+">>> Presiona ENTER para continuar <<<", "blue"))
+        input((F">>> Presiona ENTER para continuar <<<"))
 
 (iniciar())
